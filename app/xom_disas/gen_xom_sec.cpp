046b806b14997d714affb4fc4a01bbaf49775ab6
DalvikArt <ezicomond@gmail.com>
2023-11-28 00:19:11 -0600

    update

--- app/xom_disas/gen_xom_sec.cpp
+++ app/xom_disas/gen_xom_sec.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <fstream>
 
+#include "elf/elfmap.h"
 #include "analysis/controlflow.h"
 #include "conductor/setup.h"
 #include "conductor/conductor.h"
@@ -24,6 +25,21 @@ int main(int argc, char *argv[])
     const char *binaryFile = argv[1];
     const char *outputFile = argv[2];
 
+    // get .text section size
+    address_t textStartAddr = 0, textEndAddr = 0;
+
+    ElfMap *elf = new ElfMap(binaryFile);
+    auto section = elf->findSection(".text");
+    if(section)
+        textStartAddr = section->getVirtualAddress();
+        textEndAddr = textStartAddr + section->getSize();
+    else
+    {
+        std::cerr << "Failed to locate .text section for " << binaryFile << " !" << std::endl;
+        exit(0);
+    }
+
+    // start disassembling
     std::cout << "Start disassembling " << binaryFile << "..." << std::endl;
 
     ConductorSetup setup;
@@ -48,6 +64,12 @@ int main(int argc, char *argv[])
     // start building log str
     std::ostringstream oss;
 
+    // .text address
+    // StartAddress | EndAddress
+    oss << "0x" << std::hex << std::setw(8) << std::setfill('0') << textStartAddr << "|"
+        << "0x" << std::hex << std::setw(8) << std::setfill('0') << textEndAddr << std::endl;
+
+    // function addresses
     for(auto func : funcList) 
     {
         auto startAddr = func->getAddress();
